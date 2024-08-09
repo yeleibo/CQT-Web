b7f67286f4e10ba608b903c86a14757a00f0c1e7
dandelion <91513038+windy-Dandelion@users.noreply.github.com>
2024-08-09 13:42:54 +0800

    11

---
+++ src/pages/topology/FatBox.tsx
@@ -0,0 +1,13 @@
+import { ChaoqianBoxDto } from '@/pages/topology/type';
+import React from 'react';
+
+interface FatBoxProps {
+  fatBox: ChaoqianBoxDto;
+  fatBoxes: ChaoqianBoxDto[];
+}
+
+const FatBox: React.FC<FatBoxProps> = (props) => {
+  return <></>;
+};
+
+export default FatBox;
